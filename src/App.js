af3e0cff1ca15e0b4605512611384025c392cf6b
vsr4493@gmail.com <vsr4493@gmail.com>
2017-09-26 02:31:58 +0530

    Cleaned up UI

--- src/App.js
+++ src/App.js
@@ -3,49 +3,85 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import vis from 'vis';
 import './App.css';
-import Task from './Task/index';
+import Event from './Event/index';
 import Goal from './Goal/index';
 import '../node_modules/vis/dist/vis.min.css';
 var tasks = [
   {
     id: 1,
-    content: "Task One",
     group: 1,
+    subgroup: 1,
+    content: {
+      type: "Phone",
+      eventInfo: {
+        "Call Type": "Emergency",
+        "Date/Time": "04-09-2017T04:04:00",
+        "Duration" : 20,
+        "DNIS": "",
+        "ANI": "",
+        "Provider": "",
+        "Self-Service Intent": "",
+        "Self-Service Result": "",
+        "Self-Service Exit Met SLA(Yes/No)": "Yes"
+      },
+      vote: "Up"
+    },
     start: new Date("04-09-2017"),
-    end: new Date("04-21-2017")
+    type: "point",
+    className: "display-item"
   },
   {
     id: 2,
     content: "Task Two",
-    group: 2,
+    group: 1,
+    subgroup: 1,
+    content: {
+      type: "Hospital",
+      eventInfo: {
+        "Event Category": "Emergency",
+        "Date/Time": "04-09-2017T04:04:00",
+        "Duration" : 20,
+        "DNIS": "",
+        "ANI": "",
+        "Provider": "",
+        "Self-Service Intent": "",
+        "Self-Service Result": "",
+        "Self-Service Exit Met SLA(Yes/No)": "Yes"
+      },
+      vote: "Down"
+    },
     start: new Date("04-05-2017"),
-    end: new Date("04-09-2017")
+    type: "point",
+    className: "display-item"
   },
   {
     id: 3,
-    content: "Task three",
     group: 1,
+    subgroup: 1,
     start: new Date("04-02-2017"),
-    end: new Date("04-05-2017")
+    type: "point",
+    content: {
+      type: "Message",
+      eventInfo: {
+        "Message Type": "Correspondence",
+        "Message Info": "Some Message Info"
+      },
+      vote: "Up"
+    },
+    className: "display-item",
   },
-  {
-    id: 4,
-    content: "Task four",
-    group: 1,
-    start: new Date("04-04-2017"),
-    end: new Date("04-07-2017")
-  }
 ]
 
 var goals = [
   {
     id: 1,
-    content: "Phase One"
+    content: "Phase One",
+    className:"vis-group-bg",
   },
-  {
+  /*{
     id: 2,
     content: "Phase Two"
-  }
+  }*/
 ]
 
 
@@ -59,7 +95,7 @@ const prep = (el) => {
     template: function (item, element) {
         if (!item) { return }
         ReactDOM.unmountComponentAtNode(element);
-        return ReactDOM.render(<Task item={item} />, element);
+        return ReactDOM.render(<Event item={item} />, element);
     },
     groupTemplate: function(group,element){
       if(!group) {return}
---
+++ src/Event/index.js
@@ -0,0 +1,70 @@
+import {default as React} from 'react';
+import './styles.css';
+import FaHospitalO from 'react-icons/lib/fa/hospital-o';
+import FaPhone from 'react-icons/lib/fa/phone';
+import FaEnvelope from 'react-icons/lib/fa/envelope';
+import FaSmileO from 'react-icons/lib/fa/smile-o';
+import FaFrownO from 'react-icons/lib/fa/frown-o';
+
+const iconComponentByType = {
+	"Hospital": FaHospitalO,
+	"Message" : FaEnvelope,
+	"Phone" : FaPhone
+};
+
+const voteByType = {
+	"Up" : FaSmileO,
+	"Down" : FaFrownO
+}
+
+const defaultIcon = () => (<i></i>);
+
+const formatDate = (date) => {
+	let dateParsed = `${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear().toString().substring(2)}`;
+	let timeParsed = `${date.getHours()}:${date.getMinutes()}`;
+	return `${dateParsed} ${timeParsed}`;
+}
+
+const EventInfo = ({info,isVisible}) => {
+	if(isVisible){
+		return (
+			<div className="display-item-pop-over">
+				{Object.keys(info).map((key) => {
+					if(info.hasOwnProperty(key)){
+						return <div>{key} : {info[key]}</div>
+					}
+				})}
+			</div>
+		);
+	}else{
+		return <span></span>;
+	}
+}
+
+class Event extends React.Component{
+	render(){
+		let {item, item:{content}} = this.props;
+		let {type = ""} = content;
+		let {vote = ""} = content; 
+		let Icon = iconComponentByType[type] || defaultIcon;
+		let VoteIcon = voteByType[vote] || defaultIcon;
+		return (
+			<div className="display-item-container">
+				<div className="display-item-header">
+					{formatDate(item.start)}
+				</div>
+				<div className="display-item-body">
+					<Icon className="item-icon-style"/>
+					<VoteIcon 
+						className="emoji-icon-style" 
+						style={{color: vote=="Up"?"#2E7D32":"#c62828"}}
+					/>
+				</div>
+				<EventInfo info={content.eventInfo || []}/>
+			</div>
+		);
+	}
+	
+}
+
+export default Event;
